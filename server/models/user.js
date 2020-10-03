9fba9061f159952e7d2da3fff59dc5a778446cf1
Collin Lanier <collinlanier12@gmail.com>
2020-10-03 18:47:37 -0400

    set up for sequelize db, created User table

--- server/models/user.js
+++ server/models/user.js
@@ -1,13 +1,43 @@
-class User {
-  constructor({ id, email, password }) {
-    this.id = id;
-    this.email = email;
-    this.password = password;
-  }
-
-  comparePassword(challenge) {
+module.exports = function (sequelize, DataTypes) {
+  const User = sequelize.define("User", {
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true
+      }
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    is_student: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false
+    },
+    is_tutor: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false
+    },
+    timezone: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    user_bio: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  });
+  User.prototype.comparePassword = function (challenge) {
     return this.password === challenge;
   }
-}
+  return User;
+};
 
-module.exports = User;
+
