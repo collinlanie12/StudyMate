a7d50d791f48848ff6e711cc2df186ab1a817dc0
Gino Zaccardelli <ginozacc@gmail.com>
2020-10-06 13:04:30 -0400

    added frontend API routes, updated backend routes, removed force from server.js

--- server/controllers/api/postsController.js
+++ server/controllers/api/postsController.js
@@ -3,7 +3,7 @@ const postsController = require('express').Router();
 const db = require('../../models');
 
 postsController.post('/create', (req, res) => {
-    const { title, content, time , subjectId} = req.body;
+    const { title, content, time, subjectId } = req.body;
 
     db.post.create({ title, content, time, subjectId })
         .then(post => res.json(post))
@@ -24,7 +24,7 @@ postsController.get("/subject/:subjectId", (req, res) => {
     db.post.findAll({
         include: [{
             model: db.subject,
-            where: { id: req.params.subjectId } 
+            where: { id: req.params.subjectId }
         }]
     }).then(result => {
         res.json(result);
--- server/controllers/api/subjectsController.js
+++ server/controllers/api/subjectsController.js
@@ -3,7 +3,7 @@ const subjectsController = require('express').Router();
 const { JWTVerifier } = require('../../lib/passport');
 const db = require('../../models');
 
-subjectsController.get('/', JWTVerifier, (req, res) => {
+subjectsController.get('/', (req, res) => {
   db.subject.findAll()
     .then(subjects => res.json(subjects))
     .catch(err => console.log(err));
