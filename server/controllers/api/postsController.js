240e3bcc3275fd0fc4c2cb04bc3e0bc82a0fc089
Gino Zaccardelli <ginozacc@gmail.com>
2020-10-08 00:15:38 -0400

    user signup add/remove routes

--- server/controllers/api/postsController.js
+++ server/controllers/api/postsController.js
@@ -1,4 +1,5 @@
 const postsController = require('express').Router();
+const { JWTVerifier } = require('../../lib/passport');
 
 const db = require('../../models');
 
@@ -35,19 +36,33 @@ postsController.get("/subject/:subjectId", (req, res) => {
 });
 
 //new user signup to post
-postsController.post("/signup", JWTVerifier, (req, res) => {
-    Post.setUsers(req.body.userId)
-})
-    .then(result => {
-        res.json(result);
-    });
+postsController.post("/signup/add", (req, res) => {
+    db.Post.findOne({
+        where: { id: req.body.id }
+    })
+        .then(result => {
+            return Promise.all([
+                result.setUsers(req.body.UserId)
+            ])
+        })
+        .then(result => {
+            res.json(result);
+        });
+});
 
 //remove user from signup to post
-postsController.post("/signup", JWTVerifier, (req, res) => {
-    Post.removeUsers(req.body.userId)
-})
-    .then(result => {
-        res.json(result);
-    });
+postsController.delete("/signup/remove", JWTVerifier, (req, res) => {
+    db.Post.findOne({
+        where: { id: req.body.id }
+    })
+        .then(result => {
+            return Promise.all([
+                result.removeUsers(req.body.UserId)
+            ])
+        })
+        .then(result => {
+            res.json(result);
+        });
+});
 
 module.exports = postsController;
