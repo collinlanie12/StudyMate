9db5bcfe59d4d1fe95fe1aa59a79d1bd70260e42
Gino Zaccardelli <ginozacc@gmail.com>
2020-10-07 20:51:16 -0400

    working on post controller

--- server/controllers/api/postsController.js
+++ server/controllers/api/postsController.js
@@ -4,9 +4,9 @@ const db = require('../../models');
 
 //create a post
 postsController.post('/create', (req, res) => {
-    const { title, content, time, subjectId } = req.body;
+    const { title, content, time, subjectId, userId } = req.body;
 
-    db.post.create({ title, content, time, subjectId })
+    db.post.create({ title, content, time, subjectId, userId })
         .then(post => res.json(post))
         .catch(err => res.json(err));
 });
@@ -33,5 +33,13 @@ postsController.get("/subject/:subjectId", (req, res) => {
         res.json(result);
     });
 });
+
+//new user signup to post
+postsController.post("/signup", JWTVerifier, (req, res) => {
+    Post.setUsers(req.body.userId)
+})
+    .then(result => {
+        res.json(result);
+    });
 
 module.exports = postsController;
