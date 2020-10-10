99d55907817907348776776166b3f73e72d2d0db
Jordan Triplett <JordanT111596@gmail.com>
2020-10-09 22:27:39 -0400

    Added refresh posts button on 10 sec timer

--- client/src/pages/Main/Main.js
+++ client/src/pages/Main/Main.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useContext } from "react";
+import { Button } from "react-bootstrap";
 //import { Redirect } from 'react-router-dom';
 import Bubble from "../../components/Toast/Toast";
 import CalTab from "../../components/CalTab/CalTab";
@@ -15,52 +16,77 @@ function Main() {
   const auth = useContext(AuthContext);
   const postCon = useContext(PostContext);
 
+  let showBtn;
+
   const [posts, setPosts] = useState([]);
+  const [isScroll, setIsScroll] = useState(false);
+
+  
+  const handleScroll = () => {
+    const timer = setTimeout(()=> {
+      setIsScroll(true);
+    }, 10000);
+    return() => clearTimeout(timer);
+  };
+
+  const handleRefresh = () => {
+    setIsScroll(false);
+  };
 
   useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
     API.Posts.getAll(auth.authToken).then((response) => {
       console.log(response.data);
       setPosts(response.data);
     });
-  }, [postCon.submitted]);
+  }, [postCon.submitted, isScroll]);
+
+  if (isScroll) {
+    showBtn = { position: "fixed", bottom: "5%", left: "25%", display: "block" }
+  } else {
+    showBtn = { position: "fixed", bottom: "5%", left: "25%", display: "none" }
+  }
 
   return (
     <div>
-    <Navigation />
-    <div className="container-fluid">
-      
-      <div className="row">
-        <div className="col-3 leftSide">
-          <SideNav />
-        </div>
+      <Navigation />
+      <div className="container-fluid">
 
-        <div className="text-center col-6 middleRow">
-          <h2 className="mt-3 mb-4">suggested for you</h2>
-          <SearchBar />
-          {posts.map((post) => (
-            <Bubble
-              userType="student"
-              subjectName={post.SubjectId}
-              content={post.content}
-              userName={post.UserId}
-              time={post.time}
-              title={post.title}
-              date={post.date}
-            />
-          ))}
-        </div>
+        <div className="row">
+          <div className="col-3 leftSide">
+            <SideNav />
+          </div>
 
-        <div className="col-3 rightSide">
-          <h1 className="text-center calTitle">Calendar</h1>
-          <CalTab />
+          <div className="text-center col-6 middleRow">
+            <h2 className="mt-3 mb-4">suggested for you</h2>
+            <SearchBar />
+            {posts.map((post) => (
+              <Bubble
+                userType="student"
+                subjectName={post.SubjectId}
+                content={post.content}
+                userName={post.UserId}
+                time={post.time}
+                title={post.title}
+                date={post.date}
+              />
+            ))}
+          </div>
+
+          <div className="col-3 rightSide">
+            <h1 className="text-center calTitle">Calendar</h1>
+            <CalTab />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-12">
+            <div style={showBtn}>
+              <Button variant="info" onClick={handleRefresh}>Refresh Posts ⟳</Button>
+            </div>
+            <PostButton />
+          </div>
         </div>
       </div>
-      <div className="row">
-        <div className="col-12">
-          <PostButton />
-        </div>
-      </div>
-    </div>
     </div>
   );
 }
