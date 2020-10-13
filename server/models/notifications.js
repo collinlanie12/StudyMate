a90a9f15658a594a48ede3936d9c017513784fe2
Collin Lanier <collinlanier12@gmail.com>
2020-10-13 15:03:23 -0400

    removed through table for notificaitions and user

--- server/models/notifications.js
+++ server/models/notifications.js
@@ -16,9 +16,6 @@ module.exports = function (sequelize, DataTypes) {
     });
 
   Notifications.associate = models => {
-    // Notifications.belongsToMany(models.User, {
-    //   through: "user_notifications"
-    // });
     Notifications.belongsTo(models.Subject);
   };
 
